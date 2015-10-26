1231abb72a28a4ab483757ba93021cbfd3e469e2
matias <volontematias@gmail.com>
2015-10-26 14:51:55 -0400

    Deleted all projects and replaced with new demo

---
+++ ShadowShifter/Assets/Resources/Scripts/WaveGenerator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveGenerator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		SpawnWave();
+	}
+
+	void SpawnWave() {
+		GameObject obj = Instantiate(Resources.Load("Prefabs/Cube")) as GameObject;
+		obj.transform.position = new Vector3(0, 0, 7);
+		Rigidbody [] children = obj.GetComponentsInChildren<Rigidbody>();
+	    children[0].constraints = RigidbodyConstraints.FreezePosition;
+
+	}
+	
+}
