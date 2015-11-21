e1aa3a07d014c8abe8626378d24f9f0ae46ca62b
matias <volontematias@gmail.com>
2015-11-21 11:49:35 -0500

    game update
    First full pass complete

---
+++ ShadowShifter/Assets/scripts/leveOutput.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+
+public class leveOutput : MonoBehaviour {
+	public Text levelData;
+
+	// Use this for initialization
+	void Start () {
+		if(levelData!=null)
+		levelData.text = "";
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+
+	void OnGUI(){
+		if(Application.loadedLevelName =="level_01"){
+			levelData.text = "Level 1";
+		}
+		if(Application.loadedLevelName =="level_02"){
+			levelData.text = "Level 2";
+		}
+		if(Application.loadedLevelName =="level_03"){
+			levelData.text = "Level 3";
+		}
+		if(Application.loadedLevelName =="level_04"){
+			levelData.text = "Level 4";
+		}
+		if(Application.loadedLevelName =="level_05"){
+			levelData.text = "Level 5";
+		}
+		if(Application.loadedLevelName =="level_06"){
+			levelData.text = "Level 6";
+		}
+		if(Application.loadedLevelName =="level_07"){
+			levelData.text = "Level 7";
+		}
+		if(Application.loadedLevelName =="level_08"){
+			levelData.text = "Level 8";									
+		}
+		if(Application.loadedLevelName =="level_09"){
+			levelData.text = "Level 9";
+		}
+		if(Application.loadedLevelName =="level_10"){
+			levelData.text = "Level 10";
+			}
+  }
+}
--- ShadowShifter/Assets/scripts/printResult.cs
+++ ShadowShifter/Assets/scripts/printResult.cs
@@ -4,22 +4,28 @@ using UnityEngine.UI;
 
 public class printResult : MonoBehaviour {
 	public Text resultText;
+	public Text timeResult;
+
 
 	float printerTime = timeCounter.timeResult;
 
 	void Start () {
 		if(resultText!=null)
-		resultText.text = "Works";
-		print (resultText);
+		resultText.text = "result";
+		//print (resultText);
+
+		if(timeResult!=null)
+			resultText.text = "time";
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
 		printerTime = timeCounter.timeResult;
-		tester ();
+		//tester ();
 	}
-	void tester(){
+	/*void tester(){
 
 		if(printerTime<2.0f){
 			Debug.Log("less than 2 seconds: "+printerTime);
@@ -29,19 +35,21 @@ public class printResult : MonoBehaviour {
 		}
 
 
-	}
+	}*/
 
 
 	void OnGUI(){
 		
 		if(Application.loadedLevelName =="final"){
 
+			timeResult.text = printerTime + " was your time";
+
 			if(printerTime < 20.0f){
-				resultText.text = "You are the best player ever!!";
+				resultText.text = "Nice! keep improving";
 			}
 			if(printerTime > 20.0f){
 				
-				resultText.text = "You kind of suck!!";
+				resultText.text = "Sorry but you kind of suck!!";
 			}
 		}
 		
